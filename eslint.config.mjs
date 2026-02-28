4cd7b6108318bea4f40bd2371e50425b2391fd78
Tran Hai Trieu <haitrieu2524@gmail.com>
2026-02-28 19:02:22 +0700

    chore: khởi tạo dự án.

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+// @ts-check
+import eslint from '@eslint/js'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          trailingComma: 'all',
+          semi: false,
+          arrowParens: 'always',
+          tabWidth: 2,
+          endOfLine: 'auto',
+          useTabs: false,
+          printWidth: 120,
+        },
+      ],
+    },
+  },
+)
